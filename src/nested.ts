2bef5bb2303257e6ce5e464adddbf39c0f7882bd
Pokpo <qijia@udel.edu>
2025-02-24 20:12:47 -0500

    sameType done

--- src/nested.ts
+++ src/nested.ts
@@ -157,7 +157,15 @@ export function publishAll(questions: Question[]): Question[] {
  * are the same type. They can be any type, as long as they are all the SAME type.
  */
 export function sameType(questions: Question[]): boolean {
-    return false;
+    if (questions.length === 0) {
+        return true;
+    }
+    const targetType: QuestionType = questions[0].type;
+    return questions.reduce(
+        (currBool: boolean, currQ: Question) =>
+            currBool && currQ.type === targetType,
+        true,
+    );
 }
 
 /***
