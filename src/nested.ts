6bdee7e6d7a34ddf3197067396eb7471e8251b1e
Pokpo <qijia@udel.edu>
2025-02-24 19:40:20 -0500

    sumPublishedPoints

--- src/nested.ts
+++ src/nested.ts
@@ -77,7 +77,11 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    return questions.reduce(
+        (sum: number, currQ: Question) =>
+            currQ.published === true ? (sum += currQ.points) : sum,
+        0,
+    );
 }
 
 /***
